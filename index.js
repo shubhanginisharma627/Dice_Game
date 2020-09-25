c5ea5895cabf1c921832fe08a86e2a69fbc4d93c
Shubhangini Sharma <63640650+shubhanginisharma627@users.noreply.github.com>
2020-09-25 17:32:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var images = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+var images = ["dice1.png", "dice2.png", "dice3.png", "dice4.png", "dice5.png", "dice6.png"];
  
 // first dice
  
